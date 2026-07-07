7f624e6ffd55dc004e336c9161c571f6d79280cd
Hiruna Geesara <hirunagunathunga0@gmail.com>
2026-07-07 13:59:34 +0530

    compleate Dealer.java

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,2 +1,35 @@
 public class Dealer {
+    private String dealerId;
+    private String dealerName;
+    private String phoneNumber;
+    private String location;
+
+    public Dealer(String dealerId, String dealerName,
+                  String phoneNumber, String location) {
+        this.dealerId = dealerId;
+        this.dealerName = dealerName;
+        this.phoneNumber = phoneNumber;
+        this.location = location;
+    }
+
+    public String getDealerId() {
+        return dealerId;
+    }
+
+    public String getDealerName() {
+        return dealerName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String toString() {
+        return dealerId + " | " + dealerName + " | " +
+                phoneNumber + " | " + location;
+    }
 }
