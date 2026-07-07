ce073da5ad211ad950a38a683440c3371037449d
Hiruna Geesara <hirunagunathunga0@gmail.com>
2026-07-07 20:46:43 +0530

    Cartltem.java file compleating

--- src/AuditLogger.java
+++ src/AuditLogger.java
@@ -1,2 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
 public class AuditLogger {
+
+    public static void log(String action, String itemCode, int quantity) {
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("audit_log.txt", true));
+
+            pw.println(LocalDateTime.now() +
+                    " | Action: " + action +
+                    " | Item Code: " + itemCode +
+                    " | Quantity: " + quantity);
+
+            pw.close();
+
+        } catch (IOException e) {
+            System.out.println("Error writing audit log: " + e.getMessage());
+        }
+    }
 }
