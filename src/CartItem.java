6fb99fe7d8a36bfb87a11ed96606da7d4ebba6bd
Hiruna Geesara <hirunagunathunga0@gmail.com>
2026-07-16 23:48:01 +0530

    finish CartItem.java

--- src/CartItem.java
+++ src/CartItem.java
@@ -1,2 +1,38 @@
 public class CartItem {
+    private InventoryItem item;
+    private int quantity;
+
+    public CartItem(InventoryItem item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public InventoryItem getItem() {
+        return item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getSubtotalBeforeDiscount() {
+        return item.getPrice() * quantity;
+    }
+
+    public double getSubtotalAfterDiscount() {
+        double subtotal = getSubtotalBeforeDiscount();
+
+        if (quantity >= 3) {
+            subtotal = subtotal * 0.95;
+        }
+
+        return subtotal;
+    }
+
+    public String toString() {
+        return item.getPartCode() + " | " +
+                item.getPartName() + " | Qty: " +
+                quantity + " | Subtotal: Rs. " +
+                getSubtotalAfterDiscount();
+    }
 }
